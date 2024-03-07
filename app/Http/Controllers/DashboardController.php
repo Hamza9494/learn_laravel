a8defbbdc92e53eb31fc259f4d1ea5b7c30870f9
arthur-linux <baitichhamza@gmail.com>
2024-03-07 15:28:25 +0100

    learn about Models, adding entrties to DB , and migrations

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,23 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Idea;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        $users = [
-            [
-                "name" => "alex",
-                "age" => "25"
-            ],
-            [
-                "name" => "sara",
-                "age" => "16",
-            ]
-        ];
+        $idea = new Idea(["content" => "hello laravel", "likes" => 10]);
+        $idea->save();
 
-        return view("dashboard", ["users" => $users]);
+        return view("dashboard", ['ideas' => Idea::orderBy('created_at', 'DESC')->get()]);
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -51,7 +51,10 @@
             </div>
         </div>
         <hr>
+        @foreach ($ideas as $idea)
+
         <div class="mt-3">
+
             <div class="card">
                 <div class="px-3 pt-4 pb-2">
                     <div class="d-flex align-items-center justify-content-between">
@@ -67,15 +70,12 @@
                 </div>
                 <div class="card-body">
                     <p class="fs-6 fw-light text-muted">
-                        comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes
-                        of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of
-                        ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum
-                        dolor sit amet..", comes from a line in section 1.10.32.
+                        {{$idea->content}}
                     </p>
                     <div class="d-flex justify-content-between">
                         <div>
                             <a href="#" class="fw-light nav-link fs-6"> <span class="fas fa-heart me-1">
-                                </span> 100 </a>
+                                </span> {{$idea->likes}} </a>
                         </div>
                         <div>
                             <span class="fs-6 fw-light text-muted"> <span class="fas fa-clock"> </span>
@@ -102,19 +102,17 @@
                                         ago</small>
                                 </div>
                                 <p class="fs-6 mt-3 fw-light">
-                                    and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and
-                                    Evil)
-                                    by
-                                    Cicero, written in 45 BC. This book is a treatise on the theory of ethics,
-                                    very
-                                    popular during the Renaissan
-                                </p>
+                                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora amet maiores
+                                    dignissimos fugiat voluptatum veniam, praesentium consequuntur officia voluptas iste
+                                    neque, optio asperiores in, libero odio similique modi error! Nulla.
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+
+        @endforeach
     </div>
     <div class="col-3">
         <div class="card">

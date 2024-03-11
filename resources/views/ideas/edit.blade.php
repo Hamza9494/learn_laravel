44611dab7100878271ddad464769376de941ebcb
arthur-linux <baitichhamza@gmail.com>
2024-03-11 16:01:57 +0100

    keep going

--- resources/views/ideas/edit.blade.php
+++ resources/views/ideas/edit.blade.php
@@ -1,2 +1,18 @@
 @include('layout.layout')
-@include('shared.submit-idea')
+<h4> Edit yours ideas </h4>
+<div class="row">
+    <form action="{{route('ideas.store')}}" method="post">
+        @csrf
+        <div class="mb-3">
+            <textarea name="idea" class="form-control" id="idea" rows="3"> {{$idea->content}} </textarea>
+            @error('idea')
+            <span class="d-block fs-6 text-danger my-2"> {{$message}} </span>
+
+            @enderror
+        </div>
+        <div class="">
+            <button class="btn btn-dark"> Share </button>
+        </div>
+    </form>
+
+</div>

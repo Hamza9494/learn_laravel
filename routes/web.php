9323282025bb899ee13524b26a766ab4ebbb155c
arthur-linux <baitichhamza@gmail.com>
2024-03-17 16:00:30 +0100

    learned basics of grouping routes

--- routes/web.php
+++ routes/web.php
@@ -19,26 +19,22 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 
-Route::get('/ideas/{idea}', [IdeaController::class, 'show'])->name('ideas.show');
+Route::group(['prefix' => 'ideas/', 'as' => "ideas."], function () {
 
-Route::get('/register', [AuthController::class, 'register'])->name('register');
-
-Route::get('/login', [AuthController::class, 'login'])->name('login');
-
-Route::get('/ideas/{idea}/edit', [IdeaController::class, 'edit'])->name('ideas.edit')->middleware('auth');
+    Route::get('/{idea}', [IdeaController::class, 'show'])->name('show');
 
 
-Route::post('ideas/{idea}/comment', [CommentController::class, 'store'])->name('ideas.comments.store')->middleware('auth');
+    Route::group(['middleware' => ['auth'], function () {
 
-Route::post('/register', [AuthController::class, 'store']);
+        Route::get('/{idea}/edit', [IdeaController::class, 'edit'])->name('edit')->middleware('auth');
 
-Route::post('/login', [AuthController::class, 'authenticate']);
+        Route::post('/{idea}/edit', [IdeaController::class, 'update'])->name('update')->middleware('auth');;
 
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+        Route::delete('/{idea}', [IdeaController::class, 'destroy'])->name('destroy')
+            ->middleware('auth');
 
-Route::post('/ideas', [IdeaController::class, 'store'])->name('ideas.store')->middleware('auth');
+        Route::post('', [IdeaController::class, 'store'])->name('store')->middleware('auth');
 
-Route::post('/ideas/{idea}/edit', [IdeaController::class, 'update'])->name('ideas.update')->middleware('auth');;
-
-
-Route::delete('/ideas/{idea}', [IdeaController::class, 'destroy'])->name('ideas.destroy')->middleware('auth');
+        Route::post('/{idea}/comment', [CommentController::class, 'store'])->name('comments.store')->middleware('auth');
+    }]);
+});

aafaf27f0a2095d9b7365cf514d17f3ea6b53ec1
arthur <baitichhamza@gmail.com>
2024-03-18 18:28:07 +0100

    retore old routes

--- routes/web.php
+++ routes/web.php
@@ -20,17 +20,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 
 
-Route::get('/ideas/{idea}', [IdeaController::class, 'show'])->name('show');
+Route::get('/ideas/{idea}', [IdeaController::class, 'show'])->name('ideas.show');
 
-Route::get('/ideas/{idea}/edit', [IdeaController::class, 'edit'])->name('edit')->middleware('auth');
+Route::get('/ideas/{idea}/edit', [IdeaController::class, 'edit'])->name('ideas.edit')->middleware('auth');
 
-Route::post('/ideas/{idea}/edit', [IdeaController::class, 'update'])->name('update')->middleware('auth');;
+Route::post('/ideas/{idea}/edit', [IdeaController::class, 'update'])->name('ideas.update')->middleware('auth');;
 
-Route::delete('/ideas/{idea}', [IdeaController::class, 'destroy'])->name('destroy')->middleware('auth');
+Route::delete('/ideas/{idea}', [IdeaController::class, 'destroy'])->name('ideas.destroy')->middleware('auth');
 
-Route::post('/ideas', [IdeaController::class, 'store'])->name('store')->middleware('auth');
+Route::post('/ideas', [IdeaController::class, 'store'])->name('ideas.store')->middleware('auth');
 
-Route::post('/ideas/{idea}/comment', [CommentController::class, 'store'])->name('comments.store')->middleware('auth');
+Route::post('/ideas/{idea}/comment', [CommentController::class, 'store'])->name('ideas.comments.store')->middleware('auth');
 
 Route::get('/login', [AuthController::class, 'login'])->name('login');
 

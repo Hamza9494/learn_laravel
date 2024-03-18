04dd2ffd32a5a6c0612cb11aece0c58f0e272a83
arthur-linux <baitichhamza@gmail.com>
2024-03-18 22:31:57 +0100

    start workin on profile page also

---
+++ resources/views/users/show.blade.php
@@ -0,0 +1,2 @@
+<h1>welcome to profile page</h1>
+<h3> the current user is {{$user->name}} </h3>
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\IdeaController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,32 +21,24 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 
 
-Route::get('/ideas/{idea}', [IdeaController::class, 'show'])->name('ideas.show');
+// Route::get('/ideas/{idea}', [IdeaController::class, 'show'])->name('ideas.show');
 
-Route::get('/ideas/{idea}/edit', [IdeaController::class, 'edit'])->name('ideas.edit')->middleware('auth');
+// Route::get('/ideas/{idea}/edit', [IdeaController::class, 'edit'])->name('ideas.edit')->middleware('auth');
 
-Route::post('/ideas/{idea}/edit', [IdeaController::class, 'update'])->name('ideas.update')->middleware('auth');;
+// Route::post('/ideas/{idea}/edit', [IdeaController::class, 'update'])->name('ideas.update')->middleware('auth');;
 
-Route::delete('/ideas/{idea}', [IdeaController::class, 'destroy'])->name('ideas.destroy')->middleware('auth');
+// Route::delete('/ideas/{idea}', [IdeaController::class, 'destroy'])->name('ideas.destroy')->middleware('auth');
 
-Route::post('/ideas', [IdeaController::class, 'store'])->name('ideas.store')->middleware('auth');
+// Route::post('/ideas', [IdeaController::class, 'store'])->name('ideas.store')->middleware('auth');
 
-Route::post('/ideas/{idea}/comment', [CommentController::class, 'store'])->name('ideas.comments.store')->middleware('auth');
-
-
-Route::get('/login', [AuthController::class, 'login'])->name('login');
-
-Route::get('/register', [AuthController::class, 'register'])->name('register');
-
-Route::post('/register', [AuthController::class, 'store']);
-
-Route::post('/login', [AuthController::class, 'authenticate']);
-
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+// Route::post('/ideas/{idea}/comment', [CommentController::class, 'store'])->name('ideas.comments.store')->middleware('auth');
 
 
-//Route::resource('ideas', IdeaController::class)->except(['create', 'index', 'show'])->middleware('auth');
 
-//Route::resource('ideas', IdeaController::class)->only('show');
+Route::resource('ideas', IdeaController::class)->except(['create', 'index', 'show'])->middleware('auth');
 
-// Route::resource('ideas.comments', CommentController::class)->only('store')->middleware('auth');
+Route::resource('ideas', IdeaController::class)->only('show');
+
+Route::resource('ideas.comments', CommentController::class)->only('store')->middleware('auth');
+
+Route::resource('users', UserController::class)->except(['index', 'create', 'destroy'])->middleware('auth');
